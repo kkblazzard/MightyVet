0c51a318de943dc120a17a3c635483bbd00728ac
DorianLatchague <dorian.latchague@hotmail.com>
2019-03-01 14:47:39 -0800

    Changed db name and other inconsnsistencies

--- server/users/models.js
+++ server/users/models.js
@@ -1,7 +1,7 @@
 var mongoose=require('mongoose');
 var uniqueValidator = require('mongoose-unique-validator');
 
-mongoose.connect('mongodb://localhost:27017/NGOplusplusdb', function(err){
+mongoose.connect('mongodb://localhost:27017/MightyVet', function(err){
         console.log("Connected to DB");
         if(err){console.log(err);
         }
@@ -32,7 +32,7 @@ var meetingSchema = new mongoose.Schema({
         mentor: userSchema,
         mentee: userSchema,
         date: Date,
-        time: Time,
+        time: String,
 })
 
 userSchema.plugin(uniqueValidator);
