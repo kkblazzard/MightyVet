bd53edda48ebf8ee501c2c959b3f05f0fbc8f5ef
DorianLatchague <dorian.latchague@hotmail.com>
2019-03-02 17:13:39 -0800

    added month selection to calendar

--- public/src/app/scheduling/scheduling.component.ts
+++ public/src/app/scheduling/scheduling.component.ts
@@ -1,20 +1,5 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
-import { CalendarEvent, CalendarMonthViewDay } from 'angular-calendar';
-
-const colors: any = {
-  red: {
-    primary: '#ad2121',
-    secondary: '#FAE3E3'
-  },
-  blue: {
-    primary: '#1e90ff',
-    secondary: '#D1E8FF'
-  },
-  yellow: {
-    primary: '#e3bc08',
-    secondary: '#FDF1BA'
-  }
-};
+import { CalendarView } from 'angular-calendar';
 @Component({
   selector: 'app-scheduling',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -22,35 +7,12 @@ const colors: any = {
   styleUrls: ['./scheduling.component.css']
 })
 export class SchedulingComponent implements OnInit {
+  view: CalendarView = CalendarView.Month;
+  CalendarView = CalendarView;
   viewDate: Date = new Date();
 
-  events: Array<CalendarEvent<{ incrementsBadgeTotal: boolean }>> = [
-    {
-      title: 'Increments badge total on the day cell',
-      color: colors.yellow,
-      start: new Date(),
-      meta: {
-        incrementsBadgeTotal: true
-      }
-    },
-    {
-      title: 'Does not increment the badge total on the day cell',
-      color: colors.blue,
-      start: new Date(),
-      meta: {
-        incrementsBadgeTotal: false
-      }
-    }
-  ];
   constructor() { }
 
   ngOnInit() {
-  }
-  beforeMonthViewRender({ body }: { body: CalendarMonthViewDay[] }): void {
-    body.forEach(day => {
-      day.badgeTotal = day.events.filter(
-        event => event.meta.incrementsBadgeTotal
-      ).length;
-    });
   }
 }
