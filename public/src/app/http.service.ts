4e17d827e02547cae5936132f03a8c979d861b8c
Kay Sugiyama <keifuku.sugiyama@gmail.com>
2019-03-05 16:36:39 -0800

    database updates

--- public/src/app/http.service.ts
+++ public/src/app/http.service.ts
@@ -14,14 +14,24 @@ export class HttpService {
     return this._http.post('/api/users/login', user);
   }
 // -------------------users------------------------------
-userUpdate(id, userUpdate) {
-  console.log("http.service userupdate", userUpdate);
-  return this._http.put('/api/users/' + id, userUpdate);
-}
-getUser(id) {
-  console.log("httpSErvice getting user", id);
-  return this._http.get(`/api/users/${id}`);
-}
+  addUser(newUser) {
+    console.log("http.service addUser", newUser);
+    return this._http.post('/api/users', newUser);
+  }
+  userUpdate(id, userUpdate) {
+    console.log("http.service userupdate", userUpdate);
+    return this._http.put('/api/users/' + id, userUpdate);
+  }
+  getUser(id) {
+    console.log("httpSErvice getting user", id);
+    return this._http.get(`/api/users/${id}`);
+  }
+  deleteUser(id) {
+    return this._http.delete('/api/users/' + id);
+  }
+
+//------------------donations--------------------------
+
 
 
 }
--- server/users/routes.js
+++ server/users/routes.js
@@ -7,9 +7,4 @@ module.exports=function(app){
     .put('/api/users/:id', controller.userUpdate)
     .delete('/api/users/:id', controller.userRemove)
     .post('/api/users/login', controller.userLogin)
-    .get('/api/meetings', controller.meetingsAll)
-    .post('/api/meetings', controller.meetingsNew)
-    .get('/api/meetings/:id', controller.meetingsDetails)
-    .put('/api/meetings/:id', controller.meetingsUpdate)
-    .delete('/api/meetings/:id', controller.meetingsRemove)
 }
