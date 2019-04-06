b43ce9086bacf99c35e73018c8ff3f74924f3770
DorianLatchague <dorian.latchague@hotmail.com>
2019-04-06 09:08:39 -0700

    updated mentor model, webinar model, admin-webinar page, mentorship page based on yesterday's meeting

--- server/mentors/models.js
+++ server/mentors/models.js
@@ -13,10 +13,15 @@ var MenteeSchema = new mongoose.Schema({
 });
 var MentorSchema = new mongoose.Schema({
         user: UserSchema, 
-        strengths: [String], 
+        resume: {type: String,
+                required: [true, "Please enter your resume."],
+                // minlength: [100, "Your resume must be at least 100 characters long."],
+                // maxlength: [400, "Your resume must be at most 400 characters long."]
+        }, 
         approval: {type: Boolean,
                 default: false}, //admin approval
         mentees: [MenteeSchema], //array of mentees
+        support_categories: [String], //Mental Health, Financial Advice, Career Advice and Technical or Surgical
         availabilities: [MeetingSchema] //holds an array of dates and arrays of time
 }, {timestamps:true})
 
