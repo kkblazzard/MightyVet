d6c36162f6de215b08e9f1e16ff940810f94d07d
Robin Guan <hello@robinguan.com>
2019-04-25 01:07:52 -0700

    profile page updates

--- public/src/app/user-profile/user-profile.component.ts
+++ public/src/app/user-profile/user-profile.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { FileUploadService } from '../http_services/file-upload.service';
 import { AuthenticationService } from '../http_services/authentication.service';
-import { UsersService } from '../http_services/users.service'
+import { UsersService } from '../http_services/users.service';
 import { Router } from '@angular/router';
 import { MenteesService } from '../http_services/mentees.service';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
@@ -9,7 +9,7 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 class ImageSnippet {
     pending: boolean = false;
     status: string = 'init';
-    constructor(public src: string, public file: File) {}
+    constructor(public src: string, public file: File) { }
 }
 @Component({
     selector: 'app-user-profile',
@@ -25,7 +25,7 @@ export class UserProfileComponent implements OnInit {
     newsletter: boolean;
     states: any = ['AL', 'AK', 'AS', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'DC', 'FM', 'FL', 'GA', 'GU', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MH', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'MP', 'OH', 'OK', 'OR', 'PW', 'PA', 'PR', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VI', 'VA', 'WA', 'WV', 'WI', 'WY', 'Other'];
     modal: any;
-    userInfo: any = {picture: "https://s3-us-west-1.amazonaws.com/mightyvet-test/images/profile_images/profile-image-placeholder.png"};
+    userInfo: any = { picture: "https://s3-us-west-1.amazonaws.com/mightyvet-test/images/profile_images/profile-image-placeholder.png"};
     mentees: any;
     mentee_applications: any;
     continuingEducationContent = "PROGRESS";
@@ -51,51 +51,50 @@ export class UserProfileComponent implements OnInit {
         }
         this.getUserInfo();
     }
-    open(str){
-        if(str === 'edit'){
-            this.modal = this._modalsService.open(this.edit);
+    open(str) {
+        if (str === 'edit') {
+            this.modal = this._modalsService.open(this.edit, { size: 'lg' });
         }
-        if(str === 'picture'){
+        if (str === 'picture') {
             this.modal = this._modalsService.open(this.picture);
         }
         this.modal.result.then(() => { }, () => this.closedModal());
     }
-    closedModal(){
-        this.image = "";
+    closedModal() {
+        this.image = '';
         this.getUserInfo();
     }
     processFile(imageInput: any) {
         const file: File = imageInput.files[0];
         const reader = new FileReader();
-    
+
         reader.addEventListener('load', (event: any) => {
 
-        this.fileToUpload = new ImageSnippet(event.target.result, file);
-        let obs = this._filesUploadService.userUploadImage(this.fileToUpload.file)
-        obs.subscribe(
-        (data) => {
-            this.image = data['imageUrl'];
-        },
-        (err) => {
-        
-        })
-    });
+            this.fileToUpload = new ImageSnippet(event.target.result, file);
+            let obs = this._filesUploadService.userUploadImage(this.fileToUpload.file);
+            obs.subscribe(
+                (data) => {
+                    this.image = data['imageUrl'];
+                },
+                (err) => {
 
-    reader.readAsDataURL(file);
+                });
+        });
+        reader.readAsDataURL(file);
     }
-    approveMentee(id){
+    approveMentee(id) {
         let obs = this._menteesService.menteeApproval(id);
         obs.subscribe(data => {
             console.log(data);
             this.getUserInfo();
-        })
+        });
     }
-    declineMentee(id){
-        let obs = this._menteesService.menteeDecline(this.userInfo.mentor_id._id ,id);
+    declineMentee(id) {
+        let obs = this._menteesService.menteeDecline(this.userInfo.mentor_id._id, id);
         obs.subscribe(data => {
             console.log(data);
             this.getUserInfo();
-        })
+        });
     }
     isLoggedIn() {
         return this._authenticationsService.isLoggedIn();
@@ -113,26 +112,26 @@ export class UserProfileComponent implements OnInit {
                 state: data['state'],
                 title: data['title'],
                 org: data['org']
-            }
-            if(data['mentor_id']){
-                var mentees = data['mentor_id']['mentees']
-                for(let mentee of mentees){
+            };
+            if (data['mentor_id']) {
+                var mentees = data['mentor_id']['mentees'];
+                for (let mentee of mentees) {
                     mentee.approval ? this.mentees.push(mentee) : this.mentee_applications.push(mentee);
                 }
             }
-        })
+        });
     }
-    editImage(){
+    editImage() {
         this.img_error = null;
         let obs = this._usersService.updateImage(this.userInfo._id, this.image);
         obs.subscribe(data => {
-            if (data['errors']){
+            if (data['errors']) {
                 this.img_error = data['errors'].picture.message;
             }
-            else{
+            else {
                 this.modal.dismiss('success');
             }
-        })
+        });
     }
     // this will eventually go and update the userInfo after some validations.
 }
